72fdeea7cd4759886f989c39b767fbfed8036f2f
Jesse Wenger <wengerj@gmail.com>
2021-02-24 23:07:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS = [
   
   def won?(board)
     WIN_COMBINATIONS.select do |win_combination|
-      if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X" || board[win_combination[0]] == "O" &&  board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+      board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X" || board[win_combination[0]] == "O" &&  board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
       else
         false
       end
