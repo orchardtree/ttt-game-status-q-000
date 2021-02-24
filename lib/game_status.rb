19ca7d55d1b1915552e852cc4c4985a9226fffc2
Jesse Wenger <wengerj@gmail.com>
2021-02-24 22:59:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,29 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+  
+  def won?(board)
+    win = WIN_COMBINATIONS.select do |win_combination|
+      board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X" || board[win_combination[0]] == "O" &&  board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+    end
+      
+        
+    #if board.all?{|position| position == " " || position == nil}
+      #false
+    #elsif board.all?{|position| position == "X" || position == "O"}
+      #false
+    #else
+      #true
+    #end
+   
+  end
